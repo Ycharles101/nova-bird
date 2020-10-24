832a94b69005e1fb42a4ce00cb85f304d2fe5550
Ycharles101 <64796143+Ycharles101@users.noreply.github.com>
2020-10-24 09:48:56 +0800

    Create test.c
    My first program, just for test!

---
+++ test.c
@@ -0,0 +1,18 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+  int num1,num2;
+  cin>>num1>>num2;
+  int sum=0;
+  sum==func(num1,num2);
+  cout<<sum<<endl;
+  return 0;
+}
+
+int func(int x,int y)
+{
+  return x*y;
+}
